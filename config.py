60e88183ec84c87012ed4c336a30ee3f9d2838fc
franckdetin-droid <franckdetin@gmail.com>
2026-07-20 07:58:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 import os
 import cloudinary
 
-# Configuration Cloudinary (sécurisée avec variables d'environnement)
+
+# ==========================
+# CLOUDINARY
+# ==========================
+
 cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET")
+    cloud_url=os.environ.get("CLOUDINARY_URL")
 )
 
+
+# ==========================
+# CONFIGURATION FLASK
+# ==========================
 
 class Config:
 
@@ -17,6 +23,7 @@ class Config:
         "change-moi-cette-cle-secrete"
     )
 
+
     # Base de données Supabase PostgreSQL
     SQLALCHEMY_DATABASE_URI = (
         "postgresql+pg8000://postgres.mctfgwglsiwtogiwjxio:"
@@ -24,8 +31,10 @@ class Config:
         "@aws-0-eu-north-1.pooler.supabase.com:5432/postgres"
     )
 
-    # Désactive les notifications inutiles de SQLAlchemy
+
+    # Désactive les notifications SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Taille maximale des fichiers (100 Mo)
+
+    # Taille maximale des fichiers uploadés (100 Mo)
     MAX_CONTENT_LENGTH = 100 * 1024 * 1024
