bbaefe35cac853d5615ce7cc13360b2bec0e5085
franckdetin-droid <franckdetin@gmail.com>
2026-07-21 14:04:00 +0100

    Update config.py

--- server/config.py
+++ server/config.py
@@ -3,33 +3,20 @@ import os
 
 class Config:
 
-    # ==========================
-    # FLASK SECRET KEY
-    # ==========================
-
     SECRET_KEY = os.environ.get(
         "SECRET_KEY",
         "change-moi-cette-cle-secrete"
     )
 
 
-    # ==========================
-    # DATABASE SUPABASE POSTGRES
-    # ==========================
-
+    # Supabase PostgreSQL
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "postgresql+psycopg://postgres.iokvvjkgehewlszzkcvb:"
-        "franckdetin2012"
-        "@aws-0-eu-west-1.pooler.supabase.com:6543/postgres"
-        "?pgbouncer=true"
+        "DATABASE_URL"
     )
 
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-    # Reconnexion automatique si Supabase coupe la connexion
 
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
@@ -39,44 +26,23 @@ class Config:
     }
 
 
-    # ==========================
-    # STOCKAGE FICHIERS
-    # ==========================
-
+    # Upload
     UPLOAD_FOLDER = "storage"
-
-
-    # Taille maximale upload : 5 Go
 
     MAX_CONTENT_LENGTH = (
         5 * 1024 * 1024 * 1024
     )
 
 
-    # ==========================
-    # CLOUDINARY
-    # ==========================
-
+    # Cloudinary
     CLOUDINARY_CLOUD_NAME = os.environ.get(
         "CLOUDINARY_CLOUD_NAME"
     )
-
 
     CLOUDINARY_API_KEY = os.environ.get(
         "CLOUDINARY_API_KEY"
     )
 
-
     CLOUDINARY_API_SECRET = os.environ.get(
         "CLOUDINARY_API_SECRET"
     )
-
-
-    # ==========================
-    # SESSION
-    # ==========================
-
-    SESSION_COOKIE_SECURE = True
-    SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = "Lax"
-    
