5d59b7c708d8bbc0d5a89545e7b69bf7ec0dc643
franckdetin-droid <franckdetin@gmail.com>
2026-07-21 12:28:30 +0100

    Refactor database URI and add upload folder config

--- config.py
+++ config.py
@@ -12,26 +12,22 @@ class Config:
 
     # Base de données Supabase PostgreSQL
     SQLALCHEMY_DATABASE_URI = (
-        "postgresql+psycopg2://postgres.mctfgwglsiwtogiwjxio:"
+        "postgresql+pg8000://postgres.mctfgwglsiwtogiwjxio:"
         "n6em*QhKz8RSAHD"
         "@aws-0-eu-north-1.pooler.supabase.com:5432/postgres"
     )
-    SQLALCHEMY_ENGINE_OPTIONS = {
-    "pool_pre_ping": True,
-    "pool_recycle": 300
-    }
 
 
     # Désactive les notifications inutiles de SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
+    # Dossier des fichiers uploadés
+    UPLOAD_FOLDER = "storage"
+
+
     # Taille maximale des fichiers (5 Go)
-MAX_CONTENT_LENGTH = 5 * 1024 * 1024 * 1024
-
-
-
-
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024 * 1024
 
 
     # ==========================
@@ -39,18 +35,15 @@ MAX_CONTENT_LENGTH = 5 * 1024 * 1024 * 1024
     # ==========================
 
     CLOUDINARY_CLOUD_NAME = os.environ.get(
-        "CLOUDINARY_CLOUD_NAME",
-        "nf8uv7f4"
+        "CLOUDINARY_CLOUD_NAME"
     )
 
 
     CLOUDINARY_API_KEY = os.environ.get(
-        "CLOUDINARY_API_KEY",
-        "921943687156619"
+        "CLOUDINARY_API_KEY"
     )
 
 
     CLOUDINARY_API_SECRET = os.environ.get(
-        "CLOUDINARY_API_SECRET",
-        ""
+        "CLOUDINARY_API_SECRET"
     )
